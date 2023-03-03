a31072b3ea538cb86b55507f97d7cf2d5e73da03
Jeffery MJ <jefferymjoseph@gmail.com>
2023-03-03 04:14:53 -0800

    added a user post page and made small changes to some other pages

---
+++ scripts/firebaseAPI_BBY32.js
@@ -0,0 +1,19 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyDCsENxls9oyYo9t4Zkipd69MAZmcP1T0U",
+    authDomain: "bby32-1800.firebaseapp.com",
+    projectId: "bby32-1800",
+    storageBucket: "bby32-1800.appspot.com",
+    messagingSenderId: "421989553084",
+    appId: "1:421989553084:web:634dc7a558b073a09591b9"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
