1fda9132bca3f5135fdec592671bc2c505a61748
Jeffery MJ <jefferymjoseph@gmail.com>
2023-03-31 14:40:40 -0700

    added weather icon

--- scripts/temperature.js
+++ scripts/temperature.js
@@ -1,6 +1,11 @@
+// Set up API request URL with your API key and location
+const apiKey = '7d6d38173ae18bf5a612fcdf14342cd4';
+const city = 'Burnaby';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
 $(document).ready(function() {
-	var apiKey = '7d6d38173ae18bf5a612fcdf14342cd4';
-	var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Burnaby&appid=' + apiKey;
+	// var apiKey = '7d6d38173ae18bf5a612fcdf14342cd4';
+	// var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Burnaby&appid=' + apiKey;
 		
 	$.getJSON(apiUrl, function(data) {
 		var temperature = Math.round(data.main.temp - 273.15);
@@ -10,3 +15,14 @@ $(document).ready(function() {
 		$('#description').html(description);
 	});
 });
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+    const iconImg = document.createElement('img');
+    iconImg.src = iconUrl;
+    document.getElementById('weather-icon').appendChild(iconImg);
+  })
+  .catch(error => console.log(error));
